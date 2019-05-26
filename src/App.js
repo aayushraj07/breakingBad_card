79f527cd27f4bc401b90fe902e32d855ddd51680
aayushraj07 <aayush.97raj@gmail.com>
2019-05-26 11:39:54 +0530

    bootstrap added , done some visual changes , api code added for getting all character details,but not attached to searchfield

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor(){
+    super();
+      this.state={
+        characters : [],
+        searchField : ''
+      }
+    }
+    //
+    // getUsers = async function() {
+    //     const resp = await fetch('https://www.breakingbadapi.com/api/characters')
+    //     const users = await resp.json()
+    //     this.setState({characters:users})
+    // }
+
+    getChar = () =>{
+      axios.get(`https://www.breakingbadapi.com/api/characters?name=${this.state.searchField}`)
+           .then(response => {
+             console.log(response);
+           });
+    };
+
+    getSearch = search =>{
+      this.setState({search},() => this.getChar());
+    };
+
+
+
+  render(){
+    return(
+    <div className="container-fluid">
+      <div class="row">
+      <form id="" >
+        <button id='getChar' className="btn btn-primary mr-4">Show Characters </button>
+        <input type="submit" className="btn btn-secondary" value="Submit"/>
+      </form>
+      // </div>
     </div>
-  );
+  )
+  }
+
 }
+
 
 export default App;
